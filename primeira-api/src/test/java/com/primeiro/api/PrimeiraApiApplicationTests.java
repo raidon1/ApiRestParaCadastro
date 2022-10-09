d71d2d8d5a060f48549147f36df22c81ec19bb5f
raidon1 <raidon-77455@hotmail.com>
2022-10-09 17:10:33 -0300

    Commit Initial

---
+++ primeira-api/src/test/java/com/primeiro/api/PrimeiraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeiro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
